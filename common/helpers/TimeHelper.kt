deeee5e2502e69d61433b048a5594814f7baaf7c
Sokeriaaa <73162265+Sokeriaaa@users.noreply.github.com>
2026-01-03 10:34:24 +0800

    Add TimeHelper.kt and test.

---
+++ common/helpers/TimeHelper.kt
@@ -0,0 +1,46 @@
+/**
+ * Copyright (C) 2026 Sokeriaaa
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU Affero General Public License as published by the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+package sokeriaaa.return0.shared.common.helpers
+
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
+
+object TimeHelper {
+
+    const val ONE_HOUR = 1000L * 60L * 60L
+    const val ONE_DAY = ONE_HOUR * 24L
+    const val ONE_WEEK = ONE_DAY * 7L
+
+    @OptIn(ExperimentalTime::class)
+    fun currentTimeMillis(): Long = Clock.System.now().toEpochMilliseconds()
+
+    /**
+     * Get the time millis at 0:00 UTC the next day.
+     */
+    fun nextDay(now: Long = currentTimeMillis()): Long =
+        ((now / ONE_DAY) + 1) * ONE_DAY
+
+    /**
+     * Get the time millis at 0:00 UTC next Sunday.
+     * (January 1, 1970 was a Thursday.)
+     */
+    fun nextSunday(now: Long = currentTimeMillis()): Long {
+        val daysSinceEpoch = now / ONE_DAY
+        val dayOfWeek = (daysSinceEpoch + 4) % 7
+        val daysUntilSunday = (7 - dayOfWeek) % 7
+        return (daysSinceEpoch + daysUntilSunday) * ONE_DAY
+    }
+
+}
